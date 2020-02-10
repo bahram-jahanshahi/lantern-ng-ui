be8d243a6afa7d92b0b27c06168d41a0802ed197
Bahram Jahanshahi <jahanshahi@amnpardaz.ir>
2020-02-10 15:32:14 +0330

    Desktop Responsive

--- src/app/core/ui-layout/desktop/desktop.component.ts
+++ src/app/core/ui-layout/desktop/desktop.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {LanguageService} from '../../services/language.service';
+import {ResponsiveService} from '../../services/responsive.service';
 
 @Component({
   selector: 'app-desktop',
@@ -7,21 +9,62 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DesktopComponent implements OnInit {
 
+  isMediumWeb = true;
+  isLargeWeb = true;
   isSideBarOpened = true;
   sideNavMode = 'side';
 
-  constructor() {
+  constructor(private languageService: LanguageService, private responsiveService: ResponsiveService) {
   }
 
   ngOnInit() {
+    // Medium Observation
+    this.responsiveService.isWebMedium
+      .subscribe(matched => {
+        if (matched) {
+          this.changeSideNavMode('side');
+          this.isMediumWeb = true;
+        } else {
+          this.changeSideNavMode('over');
+          this.isMediumWeb = false;
+        }
+      });
+    // Large Observation
+    this.responsiveService.isWebLarge
+      .subscribe(matched => {
+        this.isLargeWeb = matched;
+      });
   }
 
   fa() {
+    this.languageService.fa();
   }
 
   en() {
+    this.languageService.en();
   }
 
   sideBarClicked(to: string) {
+    if (!this.isMediumWeb) {
+      this.closeSideBar();
+    }
+  }
+
+  changeSideNavMode(mode: string) {
+    this.sideNavMode = mode;
+    if (mode === 'side') {
+      this.openSideBar();
+    }
+    if (mode === 'over') {
+      this.closeSideBar();
+    }
+  }
+
+  closeSideBar() {
+    this.isSideBarOpened = false;
+  }
+
+  openSideBar() {
+    this.isSideBarOpened = true;
   }
 }
